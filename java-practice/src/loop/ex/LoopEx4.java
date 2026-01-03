a98b8afd51934909d9f6ac7c1e5d09297d87d315
jinahyun <jinahyun@jinahyunui-MacBookAir.local>
2026-01-03 21:14:58 +0900

    김영한 자바 입문 2일차

---
+++ java-practice/src/loop/ex/LoopEx4.java
@@ -0,0 +1,9 @@
+package loop.ex;
+
+public class LoopEx4 {
+    static void main(String[] args) {
+        for (int num=2, count=1; count<=10; num+=2, count++) {
+            System.out.println(num);
+        }
+    }
+}
